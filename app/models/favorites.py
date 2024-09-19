96e62419e7412d4b2bc56048652aaadf2c629e6d
Daniel Ho <daniel.ho337@gmail.com>
2024-09-19 09:50:53 -0700

    added Sqlalchemy-cheatsheet, renamed the Image to Label class in labels.py, created the favorite.py model

--- app/models/favorites.py
+++ app/models/favorites.py
@@ -1,10 +1,11 @@
 from .db import db, environment, SCHEMA
 
-class Label(db.Model):
-    __tablename__ = "labels"
+class Favorite(db.Model):
+    __tablename__ = "favorites"
 
     if environment == "production":
         __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String, nullable=False, unique=True)
+    image_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
