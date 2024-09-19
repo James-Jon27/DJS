94207944b465cac5d2fbc9a690abf76b05096f6d
Daniel Ho <daniel.ho337@gmail.com>
2024-09-19 10:18:24 -0700

    Adding comments in favorites regarding the db.relationships that will be added to user.py and image.py relevant to favorites

--- app/models/favorites.py
+++ app/models/favorites.py
@@ -9,3 +9,9 @@ class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     image_id = db.Column(db.Integer, db.ForeignKey("images.id"), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+# # Will be in the user.py model soon
+# images = db.relationship("Image", secondary=favorites, back_populates="users")
+
+# # Will be in the image.py model soon
+# users = db.relationship("User", secondary=favorites, back_populates="images")
