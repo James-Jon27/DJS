4b7282fe8d1b8b7da8bfd80b1735a6fe792c4068
Daniel Ho <daniel.ho337@gmail.com>
2024-09-19 10:32:35 -0700

    Created the label_images model

---
+++ app/models/label_images.py
@@ -0,0 +1,17 @@
+from .db import db, environment, SCHEMA
+
+class LabelImage(db.Model):
+    __tablename__ = "label_images"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    image_id = db.Column(db.Integer, db.ForeignKey("images.id"), nullable=False)
+    label_id = db.Column(db.Integer, db.ForeignKey("labels.id"), nullable=False)
+
+# # Will be in the image.py model soon
+# labels = db.relationship("Label", secondary=label_images, back_populates="images")
+
+# # Will be in the labels.py model soon
+# images = db.relationship("Image", secondary=label_images, back_populates="labels")
