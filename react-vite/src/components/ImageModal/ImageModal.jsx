b77d3cc040e47e80e7b998b7bab20e723174c507
James-Jon27 <jamescjones818@gmail.com>
2024-09-24 09:31:28 -0600

    Image Modal to dev

---
+++ react-vite/src/components/ImageModal/ImageModal.jsx
@@ -0,0 +1,17 @@
+export default function ImageModal({}) {
+
+    return (
+        <div className="imgPage">
+            <div className="imgUser">
+                <div className="img"></div>
+                <div className="userInt">
+                    <div className="prof"></div>
+                    <div className="stashDropdown"></div>
+                    <div className="favorite"></div>
+                </div>
+            </div>
+            <span className="imgInfo"></span>
+            <span className="comments"></span>
+        </div>
+    )
+}
