7a49e127bb47997e072cc86a223c03bbed263659
Sukhpreet Sidhu <sukhpreet8247@gmail.com>
2024-09-25 10:39:05 -0700

    fixed some thunks and routes for images

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -283,13 +283,13 @@ def del_label(id, labelName):
 @image_routes.route("/<string:labelName>")
 def find_by_label(labelName):
     images = Image.query.all()
-    res = []
+    res = {}
     for image in images:
         for label in image.labels:
             if label.name == labelName:
-                res.append({image.id:image.to_dict_basic()})
+                res[image.id] = image.to_dict_basic()
 
-    return {"images": res}
+    return res
 
 def create_labels(labels, image):
     labels = labels.strip()
