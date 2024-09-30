2d2cbf75f81653f02bd4405d7f0d32a290a40ce0
James-Jon27 <jamescjones818@gmail.com>
2024-09-30 11:46:23 -0600

    console.log errors

--- app/api/s3_helper.py
+++ app/api/s3_helper.py
@@ -19,21 +19,37 @@ def get_unique_filename(filename):
     return f"{unique_filename}.{ext}"
 
 def upload_file_to_s3(file, acl="public-read"):
+    filename = file.filename
+    content_type = file.content_type
     try:
         s3.upload_fileobj(
             file,
             BUCKET_NAME,
-            file.filename,
-            ExtraArgs={
-                "ACL": acl,
-                "ContentType": file.content_type
-            }
+            filename,
+            ExtraArgs={"ACL": acl, "ContentType": content_type},
         )
     except Exception as e:
         # in case the your s3 upload fails
         return {"errors": str(e)}
 
     return {"url": f"{S3_LOCATION}{file.filename}"}
+
+# def upload_file_to_s3(file, acl="public-read"):
+#     try:
+#         s3.upload_fileobj(
+#             file,
+#             BUCKET_NAME,
+#             file.filename,
+#             ExtraArgs={
+#                 "ACL": acl,
+#                 "ContentType": file.content_type
+#             }
+#         )
+#     except Exception as e:
+#         # in case the your s3 upload fails
+#         return {"errors": str(e)}
+
+#     return {"url": f"{S3_LOCATION}{file.filename}"}
 
 def remove_file_from_s3(image_url):
     # AWS needs the image file name, not the URL, 
